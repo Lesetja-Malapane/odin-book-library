533f5aa8eb0cc5fde0e9e19bd503309f29a2f9c2
Lesetja <kmalapane155@gmail.com>
2025-07-25 17:42:35 +0200

    refactored book function to use book class, added more styling

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 const myLibrary = [];
 const cards = document.querySelector('.cards');
 
-function Book(title, author, pages, read) {
-    // the constructor...
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    if (read === true) {
-        this.read = "Read."
-    } else {
-        this.read = "Unread."
+class Book {
+
+    constructor(title, author, pages, read, id) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = id;
     }
-    this.id = crypto.randomUUID();
+
+    get bookTitle() { return this.title; }
+    get bookAuthor() { return this.author; }
+    get numberOfPages() { return this.pages; }
+    get readConfirmation() { return this.read; }
+    get identity() { return this.id; }
+    
+    set bookTitle(newTitle) { this.title = newTitle; }
+    set bookAuthor(newAuthor) { this.author = newAuthor; }
+    set numberOfPages(newPages) { this.pages = newPages; }
+    set readConfirmation(confirm) { this.read = confirm; }
+    set identity(newIdentity) { this.id = newIdentity; }
 }
+
+// function Book(title, author, pages, read) {
+//     // the constructor...
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     if (read === true) {
+//         this.read = "Read."
+//     } else {
+//         this.read = "Unread."
+//     }
+//     this.id = crypto.randomUUID();
+// }
 
 function addBookToLibrary(...args) {
     // take params, create a book then store it in the array
-    let book = new Book(args[0], args[1], args[2], args[3])
+    let book = new Book(args[0], args[1], args[2], args[3], crypto.randomUUID())
     myLibrary.push(book)
 }
 
@@ -33,22 +56,29 @@ function displayBooks() {
         author.textContent = element.author;
         let pages = document.createElement('p');
         pages.textContent = `Pages: ${element.pages}.`; // Fix: show pages, not author
-        let read = document.createElement('p');
-        read.textContent = element.read;
+        // let read = document.createElement('p');
+        // read.textContent = element.read;
+
+        let readButton = document.createElement("button");
+        readButton.id = "rdbtn";
+        readButton.textContent = element.read ? "Read" : "Unread";
+        readButton.addEventListener("click", () => {
+            readButton.textContent = readButton.textContent === "Read" ? "Unread" : "Read";
+        })
 
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
-        card.appendChild(read);
+        card.appendChild(readButton);
 
         cards.appendChild(card);
     });
 }
 
-
 let createNewBookBtn = document.getElementById("createNewBook")
 
 createNewBookBtn.addEventListener('click', () => {
+    // creates a form
     let myForm = document.createElement("form");
     myForm.id = "bookForm"
 
